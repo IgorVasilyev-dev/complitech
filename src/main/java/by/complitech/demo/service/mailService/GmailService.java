021178b912491677b99d0d94e4880833633eb973
Igor Vasilyev <v-i-v-igor@yandex.ru>
2022-01-23 18:00:59 +0300

    add simple validate email

--- src/main/java/by/complitech/demo/service/mailService/GmailService.java
+++ src/main/java/by/complitech/demo/service/mailService/GmailService.java
@@ -5,6 +5,10 @@ import by.complitech.demo.service.mailService.api.IMailService;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.validation.ValidationException;
+
 public class GmailService implements IMailService {
 
     private final JavaMailSender emailSender;
@@ -16,9 +20,16 @@ public class GmailService implements IMailService {
     @Override
     public void sendRegistrationMail(User user) {
         SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
-        simpleMailMessage.setTo(user.getEmail());
-        simpleMailMessage.setSubject("Registration information");
-        simpleMailMessage.setText("You have registered, your password: " + user.getPassword());
-        emailSender.send(simpleMailMessage);
+        String mail = user.getEmail();
+        try {
+            InternetAddress internetAddress = new InternetAddress(mail);
+            internetAddress.validate();
+            simpleMailMessage.setTo(mail);
+            simpleMailMessage.setSubject("Registration information");
+            simpleMailMessage.setText("You have registered, your password: " + user.getPassword());
+            emailSender.send(simpleMailMessage);
+        } catch (AddressException e) {
+            throw new ValidationException("Incorrect email: " + mail);
+        }
     }
 }
